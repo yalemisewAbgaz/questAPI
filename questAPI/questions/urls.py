a60ea5c6f797659862c75c7eec78da3b75fa7d01
yalemisew Abgaz <yalemisew.abgaz@adaptcentre.ie>
2018-01-26 15:04:08 +0000

    new update

--- questAPI/questions/urls.py
+++ questAPI/questions/urls.py
@@ -5,9 +5,6 @@ from .views import QuestionnairePage, CreateView, DetailsView, QuestionView, \
 from django.conf import settings
 
 urlpatterns = {
-    url(r'^$', QuestionnairePage.as_view(), name='questionnaire'),
-    url(r'^questionlists/?$', CreateView.as_view(), name="create"),
-    url(r'^questionlists/(?P<pk>[0-9]+)/?$', DetailsView.as_view(), name="details"),
     url(r'^question/?$', QuestionView.as_view()),
     url(r'^question/(?P<pk>[0-9]+[-][A-Z]+[0-9]+)/?$', DetailedQuestionView.as_view()),
     url(r'^questionnaire/?$', QuestionnaireView.as_view()),
