9a9bd32932039a10b056f6cc4432dec3844ad510
yalemisew Abgaz <yalemisew.abgaz@adaptcentre.ie>
2018-04-05 11:20:25 +0100

    added to be viewed by all client machines

--- questAPI/questAPI/settings.py
+++ questAPI/questAPI/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = '!p6$_1bwp8*m#i7h*c-&u+nsk(u9tzy_usty4ds9)bbi1tupy7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
